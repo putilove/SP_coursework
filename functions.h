546982e3ffebcc7734c2c7ab29ed6449d71d2911
Victorina13 <victoria.tchekushkina@yandex.ru>
2022-06-07 08:23:05 +0400

    add regions in .h file

--- functions.h
+++ functions.h
@@ -20,6 +20,7 @@
 #include <time.h>
 #include <fstream>
 
+#pragma region scanner
 
 bool port_is_open(std::string ip, int port);
 
@@ -29,12 +30,23 @@ void scanPorts(std::string ip, int portInit, int portEnd);
 
 void scanPort(std::string ip, int port);
 
+#pragma endregion scanner
+
+#pragma region TCP
+
 void listeningTCP(int srcPort, bool isFileMessage, int dstPort);
 
 void connectTCP(std::string ip, int host, bool isFileMessage);
 
+#pragma endregion TCP
+
+#pragma region UDP
+
 void listeningUDP(int srcPort, bool isFileMessage, int dstPort);
 
 void connectUDP(std::string ip, int port, bool isFileMessage);
+
+#pragma endregion UDP
+
 
 std::ifstream::pos_type filesize(std::string file_name);
